ea8036a72ba7b11e963156f69c527f09f8f46128
ysw <397244929@qq.com>
2020-02-14 16:57:13 +0800

    auto commit

--- Ysw.WxrdSharer/Ysw.WxrdSharer/Form1.cs
+++ Ysw.WxrdSharer/Ysw.WxrdSharer/Form1.cs
@@ -15,5 +15,10 @@ namespace Ysw.WxrdSharer
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello");
+        }
     }
 }
